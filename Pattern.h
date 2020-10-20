a30da915ac728a65cfceeb690551ead6dc54802f
goodandevilcorp <nathaniel.winston.b@gmail.com>
2020-10-20 23:01:52 +1100

    Started work on Pattern

--- Pattern.h
+++ Pattern.h
@@ -7,16 +7,21 @@
 
 #include "Utility.h"
 #include "Feed.h"
+#include "Query.h"
+#include "Subvector.h"
 
 //#include "Subvector.h"
 
+template <typename T>
+class Pattern : public Feed<vector<T>> {
+public:
+    vector<vector<Subvector<T>>>
 
-class Pattern : public Feed<string> {
     Pattern() {
-
+        this->Add(&Process);
     }
 
-    void Process(string line) {
+    void Process(vector<T> line) {
 
     }
 
